75816ef26e23de740bd7708f6f9771d621837637
Harmony <harmonymukolwe@gmail.com>
2024-06-24 14:03:16 +0300

    Moves dodger

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Your code here
+const dodger = document.getElementById("dodger");
+
+// Moving dodger left
+function moveDodgerLeft() {
+  const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10);
+
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
+  }
+}
+
+document.addEventListener("keydown", function (e) {
+  if (e.key === "ArrowLeft") {
+    moveDodgerLeft();
+  }
+});
+
+// Moving dodger right
+function moveDodgerRight() {
+  const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10);
+
+if (left < (400 - 40)) {
+  dodger.style.left = `${left + 1}px`
+}
+  
+};
+
+document.addEventListener("keydown" , function(e) {
+  if(e.key === 'ArrowRight') {
+    moveDodgerRight();
+  }
+}
+)
